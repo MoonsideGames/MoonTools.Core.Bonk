9cbb1fdf31a8e1e7bf8c6303a3226f476d37bfa5
Evan Hemsley <2342303+ehemsley@users.noreply.github.com>
2020-01-01 23:57:47 -0800

    add AABB test

---
+++ Test/AABBTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using FluentAssertions;
+using MoonTools.Core.Bonk;
+using System.Numerics;
+
+namespace Tests
+{
+    public class AABBTest
+    {
+        [Test]
+        public void Overlapping()
+        {
+            var a = new AABB(new Vector2(-1, -1), new Vector2(1, 1));
+            var b = new AABB(new Vector2(0, 0), new Vector2(2, 2));
+
+            AABB.TestOverlap(a, b).Should().BeTrue();
+        }
+
+        [Test]
+        public void NotOverlapping()
+        {
+            var a = new AABB(new Vector2(-1, -1), new Vector2(1, 1));
+            var b = new AABB(new Vector2(-3, -3), new Vector2(-2, -2));
+
+            AABB.TestOverlap(a, b).Should().BeFalse();
+        }
+    }
+}
